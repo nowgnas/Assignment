4857affd83cbe403f749ebc1cd63adb225963da3
marshmello.sw@gmail.com <marshmello.sw@gmail.com>
2021-11-17 01:49:54 +0900

    fin AI Assignment3

--- AI/assignment3/main_linear_reg_mat_basis.py
+++ AI/assignment3/main_linear_reg_mat_basis.py
@@ -4,11 +4,11 @@ import numpy as np
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
-
 if __name__ == '__main__':
     # STEP 1: SET DATA ------------------------------------------------------------------------------------------------#
     data_x = np.linspace(1.0, 10.0, 100)[:, np.newaxis]
-    data_y = np.sin(data_x) + 0.1 * np.power(data_x, 2) + 0.5 * np.random.randn(100, 1)
+    data_y = np.sin(data_x) + 0.1 * np.power(data_x, 2) + 0.5 * np.random.randn(100,
+                                                                                1)
     data_x /= np.max(data_x)
 
     data_x = np.hstack((np.ones_like(data_x), data_x))
@@ -21,21 +21,41 @@ if __name__ == '__main__':
     x_train = data_x[order[portion:]]
     y_train = data_y[order[portion:]]
 
+
     # STEP 2: DO PREDICTION -------------------------------------------------------------------------------------------#
     # Nonlinear basis function
+    def get_fraction(x, mu, s):
+        x_mu = (x - mu) ** 2
+        div_x = x_mu / (2 * (s ** 2))
+        return -div_x
+
+
     def apply_exp_basis(X, s=0.5):
         # Write code here (1)!
+        out_list = []
+        mu = [0.0, 0.2, 0.4, 0.6]
+        idx_list = []
+        for i in X:
+            x = i[1]
+            idx_list.append(i[0])
+            for _mu in mu:
+                get_phi = get_fraction(x, _mu, s)
+                idx_list.append(get_phi)
+            out_list.append(idx_list)
+            idx_list = []
+        return out_list
 
-        Phi = np.copy(X)  # 코드 작성시 이부분을 지우세요!
-        return Phi
 
     phi_train = apply_exp_basis(x_train)
+    x_T = np.transpose(phi_train)
+    X_X_T = np.matmul(x_T, phi_train)
+    x_inv = inv(X_X_T)
+    x_inv_X = np.matmul(x_inv, x_T)
 
     # Write code here (2)!
 
-
     # Find theta_hat.
-    theta_hat = np.ones((2, 1))  # 코드 작성시 이부분을 지우세요!
+    theta_hat = np.matmul(x_inv_X, y_train)
 
     phi_test = apply_exp_basis(x_test)
     y_pred = np.matmul(phi_test, theta_hat)
@@ -46,6 +66,3 @@ if __name__ == '__main__':
     ax.plot(x_test[:, 1], y_pred, 'ro', label='Prediction')
     plt.legend()
     plt.show()
-
-
-
